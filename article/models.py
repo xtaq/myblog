68f66bc8fa693e1d989a1d3ce3b6c55395c6502d
向桐 <tong.xiang@mfashion.com.cn>
2016-01-06 16:09:11 +0800

    细节修改

--- article/models.py
+++ article/models.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 from django.db import models
 from collections import OrderedDict
 # Create your models here.
@@ -15,14 +15,14 @@ class Author(models.Model):
 
 class TagManager(models.Manager):
 
-    def get_Tag_list(self):#返回文章标签列表, 每个标签以及对应的文章数目
-        tags = Tag.objects.all()#获取所有标签
+    def get_Tag_list(self):  # 返回文章标签列表, 每个标签以及对应的文章数目
+        tags = Tag.objects.all()  # 获取所有标签
         tag_list = []
-        for i  in range(len(tags)):
+        for i in range(len(tags)):
             tag_list.append([])
-        for i  in range(len(tags)):
-            temp = Tag.objects.get(name = tags[i]) #获取当前标签
-            posts = temp.article_set.all() #获取当前标签下的所有文章
+        for i in range(len(tags)):
+            temp = Tag.objects.get(name=tags[i])  # 获取当前标签
+            posts = temp.article_set.all()  # 获取当前标签下的所有文章
             tag_list[i].append(tags[i].name)
             tag_list[i].append(len(posts))
         return tag_list
@@ -32,8 +32,8 @@ class Tag(models.Model):
     name = models.CharField(max_length=20,blank=True)
     creat_time = models.DateTimeField(auto_now_add=True)
 
-    objects = models.Manager()#默认的管理器
-    tag_list = TagManager()#自定义的管理器
+    objects = models.Manager()  # 默认的管理器
+    tag_list = TagManager()  # 自定义的管理器
 
 
     @models.permalink
@@ -47,14 +47,14 @@ class Tag(models.Model):
 
 class ClassManager(models.Manager):
 
-    def get_Class_list(self):#返回文章分类列表, 每个分类以及对应的文章数目
-        classf = Classification.objects.all() #获取所有的分类
+    def get_Class_list(self):  # 返回文章分类列表, 每个分类以及对应的文章数目
+        classf = Classification.objects.all()  # 获取所有的分类
         class_list = []
         for i  in range(len(classf)):
             class_list.append([])
         for i  in range(len(classf)):
-            temp = Classification.objects.get(name = classf[i]) #获取当前分类
-            posts = temp.article_set.all() #获取当前分类下的所有文章
+            temp = Classification.objects.get(name=classf[i])  # 获取当前分类
+            posts = temp.article_set.all()  # 获取当前分类下的所有文章
             class_list[i].append(classf[i])
             class_list[i].append(len(posts))
         return class_list
@@ -63,8 +63,8 @@ class ClassManager(models.Manager):
 class Classification(models.Model):
       name = models.CharField(max_length=25)
 
-      objects = models.Manager()#默认的管理器
-      class_list = ClassManager()#自定义的管理器
+      objects = models.Manager()  # 默认的管理器
+      class_list = ClassManager()  # 自定义的管理器
       
       def __unicode__(self):
           return self.name
@@ -72,10 +72,10 @@ class Classification(models.Model):
 
 class ArticleManager(models.Model):
 
-    def get_Article_onDate(self):  #实现文章的按月归档, 返回 月份以及对应的文章数  如: [[2015.5,5],[2015.4,5]] ,
-        post_date = Article.objects.dates('publish_time','month')
-        #post_date = post_date.reverse() #将post_date逆置,使之按月份递减的顺序排布
-        date_list=[]
+    def get_Article_onDate(self):  # 实现文章的按月归档, 返回 月份以及对应的文章数  如: [[2015.5,5],[2015.4,5]] ,
+        post_date = Article.objects.dates('publish_time', 'month')
+        # post_date = post_date.reverse() #将post_date逆置,使之按月份递减的顺序排布
+        date_list = []
         for i in range(len(post_date)):
             date_list.append([])
         for i in range(len(post_date)):
@@ -87,11 +87,11 @@ class ArticleManager(models.Model):
             date_list[i].append(tempNum)
         return date_list
 
-    def get_Article_OnArchive(self): #返回一个字典,一个时间点,对应一个文章列表
-        post_date = Article.objects.dates('publish_time','month')
-        #post_date = post_date.reverse()
+    def get_Article_OnArchive(self):  # 返回一个字典,一个时间点,对应一个文章列表
+        post_date = Article.objects.dates('publish_time', 'month')
+        # post_date = post_date.reverse()
 
-        post_date_article=[]
+        post_date_article = []
         for i in range(len(post_date)):
             post_date_article.append([])
 
@@ -103,21 +103,22 @@ class ArticleManager(models.Model):
 
         dicts = OrderedDict()
         for i in range(len(post_date)):
-            dicts.setdefault(post_date[i],post_date_article[i])
+            dicts.setdefault(post_date[i], post_date_article[i])
         return dicts
 
 
-class Article(models.Model):#文章
-    title = models.CharField(max_length = 100)
+class Article(models.Model):  # 文章
+    # id = models.CharField(max_lengh=25)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(Author)
-    tags = models.ManyToManyField(Tag,blank=True) #标签
-    classification = models.ForeignKey(Classification) #分类
-    content = models.TextField(blank=True, null = True)
+    tags = models.ManyToManyField(Tag, blank=True)  # 标签
+    classification = models.ForeignKey(Classification)  # 分类
+    content = models.TextField(blank=True, null=True)
     publish_time = models.DateTimeField(auto_now_add=True)
-    count = models.IntegerField(default = 0)#文章点击数,但未实现统计文章点击数的功能
+    count = models.IntegerField(default=0)  # 文章点击数,但未实现统计文章点击数的功能
 
-    objects = models.Manager()#默认的管理器
-    date_list = ArticleManager()#自定义的管理器
+    objects = models.Manager()  # 默认的管理器
+    date_list = ArticleManager()  # 自定义的管理器
 
     @models.permalink
     def get_absolute_url(self):
@@ -127,11 +128,11 @@ class Article(models.Model):#文章
             'day': self.publish_time.strftime('%d'),
             'id': self.id})
 
-    def get_tags(self):#返回一个文章对应的所有标签
+    def get_tags(self):  # 返回一个文章对应的所有标签
         tag = self.tags.all()
         return tag
 
-    def get_before_article(self):#返回当前文章的前一篇文章
+    def get_before_article(self):  # 返回当前文章的前一篇文章
         temp = Article.objects.order_by('id')
         cur = Article.objects.get(id=self.id)
         count = 0
@@ -144,7 +145,7 @@ class Article(models.Model):#文章
         if index != 0:
             return temp[index-1]
 
-    def get_after_article(self):#返回当前文章的后一篇文章
+    def get_after_article(self):  # 返回当前文章的后一篇文章
         temp = Article.objects.order_by('id')
         max = len(temp)-1
         cur = Article.objects.get(id=self.id)
@@ -161,7 +162,7 @@ class Article(models.Model):#文章
     def __unicode__(self):
         return self.title
 
-    class Meta: #按时间下降排序
+    class Meta:  # 按时间下降排序
         ordering = ['-publish_time']
 
 
--- article/views.py
+++ article/views.py
@@ -6,7 +6,7 @@ from django.template import RequestContext
 from article.models import Article, Tag, Classification
 from django.http import Http404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.contrib.syndication.views import Feed # 订阅RSS
+from django.contrib.syndication.views import Feed  # 订阅RSS
 import json
 from django.core import serializers
 
@@ -37,7 +37,7 @@ def home(request):
         context_instance=RequestContext(request))
 
 
-def detail(request, year,month,day,id):
+def detail(request, year, month, day, id):
     try:
         article = Article.objects.get(id=str(id))
     except Article.DoesNotExist:
@@ -46,7 +46,7 @@ def detail(request, year,month,day,id):
     # ar_newpost = Article.objects.order_by('-publish_time')[:10]
 
     classification = Classification.class_list.get_Class_list()
-    tagCloud = json.dumps(Tag.tag_list.get_Tag_list(),ensure_ascii=False)  # 标签,以及对应的文章数目
+    tagCloud = json.dumps(Tag.tag_list.get_Tag_list(), ensure_ascii=False)  # 标签,以及对应的文章数目
     date_list = Article.date_list.get_Article_onDate()
 
     return render_to_response('blog/content.html',
@@ -58,7 +58,7 @@ def archive_month(request, year, month):
     
     is_arch_month = True
     articles = Article.objects.filter(publish_time__year=year).filter(publish_time__month=month)  # 当前日期下的文章列表
-    paginator = Paginator(articles,6)
+    paginator = Paginator(articles, 6)
     page_num = request.GET.get('page')
     try:
         articles = paginator.page(page_num)
@@ -69,7 +69,7 @@ def archive_month(request, year, month):
 
     # ar_newpost = Article.objects.order_by('-publish_time')[:10]
     classification = Classification.class_list.get_Class_list()  
-    tagCloud = json.dumps(Tag.tag_list.get_Tag_list(),ensure_ascii=False)  # 标签,以及对应的文章数目
+    tagCloud = json.dumps(Tag.tag_list.get_Tag_list(), ensure_ascii=False)  # 标签,以及对应的文章数目
     date_list = Article.date_list.get_Article_onDate()
     
     return render_to_response('blog/index.html',
@@ -107,7 +107,7 @@ def tagDetail(request, tag):
     temp = Tag.objects.get(name=tag)  # 获取全部的Article对象
     # articles = Article.objects.filter(tags=tag)
     articles = temp.article_set.all()
-    paginator = Paginator(articles,6)
+    paginator = Paginator(articles, 6)
     page_num = request.GET.get('page')
     try:
          articles = paginator.page(page_num)
@@ -174,14 +174,14 @@ def blog_search(request):  # 实现对文章标题的搜索
     is_search = True
     # ar_newpost = Article.objects.order_by('-publish_time')[:10]
     classification = Classification.class_list.get_Class_list()    
-    tagCloud = json.dumps(Tag.tag_list.get_Tag_list(),ensure_ascii=False)  # 标签,以及对应的文章数目
+    tagCloud = json.dumps(Tag.tag_list.get_Tag_list(), ensure_ascii=False)  # 标签,以及对应的文章数目
     date_list = Article.date_list.get_Article_onDate()
 
     error = False
     if 's' in request.GET:
         s = request.GET['s']
         if not s:
-            return render(request,'blog/index.html')
+            return render(request, 'blog/index.html')
         else:
             articles = Article.objects.filter(title__icontains=s)
             if len(articles) == 0:
